02899e9ec60c364ed4547403db80883ae4ec59aa
JIrayut Khantavee <ker.koleng@hotmail.co.th>
2018-12-13 10:15:06 +0700

    first commit

---
+++ prerequisite.sh
@@ -0,0 +1,22 @@
+sudo su -
+apt-get update
+apt-cache madison docker-ce
+apt-get install     apt-transport-https     ca-certificates     curl     software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt-get update
+apt-cache madison docker-ce
+apt-get install docker-ce=17.12.1~ce-0~ubuntu
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+kubeadm init --pod-network-cidr=192.168.0.0/16
+exit
